11aee605b9be7135eedd35f79de8b62a83e8a8c4
Steven Acres <steven@swatteksystems.com>
2016-11-24 14:02:43 -0500

    Add facts stanza back

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,13 @@ end
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
 RSpec.configure do |c|
+  default_facts = {
+    puppetversion: Puppet.version,
+    facterversion: Facter.version
+  }
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
+  c.default_facts = default_facts
   c.formatter = 'documentation'
   c.mock_framework = :rspec
 end
