a0c767c7522b730205f0ae0fb9af2cccd3c4caec
Steven Acres <steven@swatteksystems.com>
2016-11-24 13:42:51 -0500

    Testing coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-utils'
+require 'rspec/mocks'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
@@ -16,14 +18,12 @@ unless RUBY_VERSION =~ %r{^1.9}
   end
 end
 
-RSpec.configure do |c|
-  default_facts = {
-    puppetversion: Puppet.version,
-    facterversion: Facter.version
-  }
-  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
-  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
-  c.default_facts = default_facts
-end
+#
+# Require all support files
+#
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
-# vim: syntax=ruby
+RSpec.configure do |c|
+  c.formatter = 'documentation'
+  c.mock_framework = :rspec
+end
